98d12ad120593676f4713f0a8097d9f5a66366b7
Syed-Osama-Hassan <48532051+Syed-Osama-Hassan@users.noreply.github.com>
2023-07-09 23:19:20 +0100

    Added hystrix support

--- movie-catalog-service/src/main/java/com/hassan/osama/syed/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/hassan/osama/syed/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -3,12 +3,13 @@ package com.hassan.osama.syed.moviecatalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableHystrix
 public class MovieCatalogServiceApplication {
 
 	public static void main(String[] args) {
@@ -23,8 +24,8 @@ public class MovieCatalogServiceApplication {
 		return new RestTemplate(clientHttpRequestFactory);
 	}
 
-	@Bean
-	public WebClient.Builder webClientBuilder() {
-		return WebClient.builder();
-	}
+//	@Bean
+//	public WebClient.Builder webClientBuilder() {
+//		return WebClient.builder();
+//	}
 }
--- movie-catalog-service/src/main/java/com/hassan/osama/syed/moviecatalogservice/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/com/hassan/osama/syed/moviecatalogservice/controller/MovieCatalogController.java
@@ -2,13 +2,14 @@ package com.hassan.osama.syed.moviecatalogservice.controller;
 
 import com.hassan.osama.syed.moviecatalogservice.model.CatalogItem;
 import com.hassan.osama.syed.moviecatalogservice.model.Movie;
+import com.hassan.osama.syed.moviecatalogservice.model.Rating;
 import com.hassan.osama.syed.moviecatalogservice.model.dto.RatingDto;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,20 +26,28 @@ public class MovieCatalogController {
     }
 
     @GetMapping("{userId}")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalogById(@PathVariable String userId) {
         RatingDto ratingDto = restTemplate.getForObject(RATING_INFO_SERVICE_URL+"foo", RatingDto.class);
 
-        return ratingDto.getRatings().stream()
-                .map(rating -> {
-                    Movie movie = restTemplate.getForObject(MOVIE_INFO_SERVICE_URL + rating.getMovieId(), Movie.class);
+        return ratingDto.getRatings()
+                .stream()
+                .map(this::getCatalogItem)
+                .collect(Collectors.toList());
+    }
+
+    private CatalogItem getCatalogItem(Rating rating) {
+        Movie movie = restTemplate.getForObject(MOVIE_INFO_SERVICE_URL + rating.getMovieId(), Movie.class);
 //                    Movie movie = webClientBuilder.build()
 //                            .get()
 //                            .uri(MOVIE_INFO_SERVICE_URL + rating.getMovieId())
 //                            .retrieve()
 //                            .bodyToMono(Movie.class)
 //                            .block();
-                    return new CatalogItem(movie.getName(), "description", rating.getRating());
-                })
-                .collect(Collectors.toList());
+        return new CatalogItem(movie.getName(), "description", rating.getRating());
+    }
+
+    public List<CatalogItem> getFallbackCatalog(@PathVariable String userId) {
+        return List.of(new CatalogItem("Fallback Response", "", 0));
     }
 }
